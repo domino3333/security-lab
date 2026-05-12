8255ce715a127eaf20893db90b95b8baa9f79b04
MINO\User <busster@naver.com>
2026-05-12 12:16:51 +0900

    init

---
+++ src/test/java/com/securitylab/SecurityLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securitylab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
