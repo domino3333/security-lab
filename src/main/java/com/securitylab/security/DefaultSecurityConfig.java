cbcb34f7c26c9304fd404e0c98d0a6b4154d999b
MINO\User <busster@naver.com>
2026-05-13 01:36:30 +0900

    세션 관리 체인 작성

--- src/main/java/com/securitylab/security/DefaultSecurityConfig.java
+++ src/main/java/com/securitylab/security/DefaultSecurityConfig.java
@@ -2,6 +2,7 @@ package com.securitylab.security;
 
 
 import com.securitylab.filter.MyFilter;
+import jakarta.servlet.http.HttpSessionEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,11 +12,15 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
 import org.springframework.web.filter.DelegatingFilterProxy;
 
 import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.security.core.userdetails.User.withUsername;
 
 @EnableWebSecurity
 @Configuration
@@ -28,7 +33,13 @@ public class DefaultSecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
 
-        return http.
+        return http.sessionManagement(
+                        sm -> sm.sessionConcurrency(
+                                sc -> sc.maximumSessions(1)
+                                        .expiredUrl("/login?expired")
+                        )
+                ).
+
                 authorizeHttpRequests(
                         (authorize) -> authorize
                                 .requestMatchers("/admin/**").hasRole("ADMIN")
@@ -42,15 +53,20 @@ public class DefaultSecurityConfig {
     }
 
     @Bean
+    HttpSessionEventPublisher httpSessionEventPublisher(){
+        return new HttpSessionEventPublisher();
+    }
+
+    @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        UserDetails user = User.withUsername("user")
+                .password(encoder.encode("password"))
                 .roles("USER")
                 .build();
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
-                .password("password")
+        UserDetails admin = User
+                .withUsername("admin")
+                .password(encoder.encode("password"))
                 .roles("ADMIN", "USER")
                 .build();
         return new InMemoryUserDetailsManager(user, admin);
