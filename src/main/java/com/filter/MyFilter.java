1ba18ac1f5d1584ac861435ee3995cebc744ff90
MINO\User <busster@naver.com>
2026-05-12 22:54:16 +0900

    MyFilter 를 만들긴 했는데 서블릿 필터체인에 등록하는 방법이 따로 있는 건지, 아니면 이 자체로도 등록이 된 건지

---
+++ src/main/java/com/filter/MyFilter.java
@@ -0,0 +1,23 @@
+package com.filter;
+
+
+import jakarta.servlet.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class MyFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        log.info("MyFilter init");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        log.info("myfilter의 doFilter진입");
+    }
+}
